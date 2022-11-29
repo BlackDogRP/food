2c58c6d4e44af9773a220530f08237f1fca47c31
BlackDog <gtaxrpnet@gmail.com>
2022-11-30 01:21:07 +0300

    modal window improvements

--- js/script.js
+++ js/script.js
@@ -109,11 +109,17 @@ tabsParent.addEventListener('click', function(event) {
         modal = document.querySelector('.modal'),
         modalClose = document.querySelector('[data-close]');
 
+
+        
+    function openModal() {
+      modal.classList.toggle('show');
+      document.body.style.overflow='hidden';
+      clearInterval(modalTimerId);
+    }
+
+    
     modalTrigger.forEach(btn => {
-      btn.addEventListener('click', function() {
-        modal.classList.toggle('show');
-        document.body.style.overflow='hidden';
-      });
+      btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
@@ -133,5 +139,16 @@ tabsParent.addEventListener('click', function(event) {
     if(e.code === "Escape" && modal.classList.contains('show')) {
       closeModal();
     }
-  })
+  });
+
+  const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if(window.pageYOffset+document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
 });
